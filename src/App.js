62d36c6d11ed8d6968285325bad318ada060e8ba
shariba thasneem <sharibathasneem@gmail.com>
2022-12-26 00:23:06 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import{useState,useEffect, useRef} from 'react';
 import './App.css';
-
 function App() {
+  const [time,settime]=useState(0)
+  const [currrentstate,setcurrentstate]=useState(0)
+  useEffect(()=>{
+    return()=> clearInterval(id.current);  
+  },[])
+  let id=useRef()
+  function handleTime(){
+    id.current=setInterval(()=>{
+    settime((prev)=>prev+1)
+  },100)
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="box">
+        <h2 className="text">Stopwatch</h2>
+        <div className="time_circle">
+          <div className="sec_circle"></div>
+          <h1 className="timedisplay">{time}</h1>
+        </div>
+        <button onClick={()=>{
+          setcurrentstate(0)
+          clearInterval(id.current)
+          }}>Stop</button>
+        <button onClick={()=>{
+            setcurrentstate(1)
+            if(currrentstate!==1){
+              handleTime()
+            }
+        }} >Start</button>
+        <button onClick={()=>{clearInterval(id.current)
+        settime(0);
+        setcurrentstate(0)
+        }}>Reset</button>
+      </div>
+
     </div>
   );
 }
